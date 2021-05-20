551bdaebe77aff1d3491fd76e0f5f9e70361a077
David Saunders <david.saunders@admin.ox.ac.uk>
2021-05-20 11:06:30 +0100

    Update extract_ext_id.py
    Open csvwriter only once

--- extract_ext_id.py
+++ extract_ext_id.py
@@ -4,39 +4,39 @@ import csv
 # Written by    David Saunders 
 # Created       19-05-2021
 
-with open('output/extids.csv', newline='', mode='w') as f:
+import time
+
+with open('output/extids2.csv', newline='', mode='w') as f:
     log_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     log_writer.writerow(['ID','Type','DOI','Reporting date','Publication date','Title','Journal','External identifiers','pubmed', 'pmc','isidoc'])
     
 
-with open('input/pubs.csv', encoding='Latin-1') as csvfile:
-            readCSV = csv.reader(csvfile, delimiter=',')
-            next (readCSV)
-            for row in readCSV:
-                extids = id = title = doi = pubmed = pmc = isidoc = None
-                id = row[0]
-                title = row[69]
-                journal = row[36]
-                type = row[1]
-                doi = row[18]
-                extids = row[23]
-                repdate = row[2]
-                pubdate = row[56]
+    with open('input/pubs.csv', encoding='Latin-1') as csvfile:
+                readCSV = csv.reader(csvfile, delimiter=',')
+                next (readCSV)
+                for row in readCSV:
+                    extids = id = title = doi = pubmed = pmc = isidoc = None
+                    id = row[0]
+                    title = row[69]
+                    journal = row[36]
+                    type = row[1]
+                    doi = row[18]
+                    extids = row[23]
+                    repdate = row[2]
+                    pubdate = row[56]
 
-                idsplit = extids.split(", ")
-                for extid in idsplit:
-                    if extid[:7]== 'pubmed:':
-                        pubmed = extid[7:]
-                    if extid[:4]== 'pmc:':
-                        pmc = extid[4:]
-                    if extid[:7]== 'isidoc:':
-                        isidoc = extid[7:]
+                    idsplit = extids.split(", ")
+                    for extid in idsplit:
+                        if extid[:7]== 'pubmed:':
+                            pubmed = extid[7:]
+                        if extid[:4]== 'pmc:':
+                            pmc = extid[4:]
+                        if extid[:7]== 'isidoc:':
+                            isidoc = extid[7:]
 
 
 
-                print(id,type,doi,repdate,pubdate,title,journal,extids,pubmed,pmc,isidoc)
-                with open('output/extids.csv', newline='',mode='a') as f:
-                    log_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    print(id,type,doi,repdate,pubdate,title,journal,extids,pubmed,pmc,isidoc)
                     try:
                         log_writer.writerow([id,type,doi,repdate,pubdate,title,journal,extids,pubmed,pmc,isidoc])  
                     except:
